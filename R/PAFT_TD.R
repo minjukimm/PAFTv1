685e3cd7c59b2f59c9a8de28eb618423a0937ac9
ka2pzy <ka2pzy@naver.com>
2023-12-08 19:56:13 +0900

    PAFT_TD.R

--- R/PAFT_TD.R
+++ R/PAFT_TD.R
@@ -22,124 +22,107 @@ library(nnls) # updated weight
 #' @examples PAFT_TD(dat=dat1,X=c("Z1","X"),dist="log_normal") # True data: log-normal with b0=1, b1=1, b2=1)
 #' @examples PAFT_TD(dat=dat4,X=c("Z1","X"),dist="log_normal") # True data: log-t(3) with b0=1, b1=1, b2=1
 #'
-PAFT_TD <- function(dat,X,initi=F,beta=NA,gab=NA,dist,tol=1.0e-5,maxiter=2000,algorithm="NLOPT_LN_COBYLA"){
-
-  ## inital values
-  if(initi==T){
-    beta <- beta
-    gab <- gab
+PAFT_TD <- function(formula,data,dist,initial=F,beta0=NA,scale0=NA,tol=1.0e-5,maxiter=2000,algorithm="NLOPT_LN_COBYLA"){
+  
+  ## Basic options
+  varnames <- all.vars(formula)
+  covr_names <- varnames[c(4:length(varnames))]
+  last_data <- as.data.frame(data%>%group_by(data[,1])%>%arrange(varnames[2])%>%slice(n()))[,c(names(data)[1],varnames[2:length(varnames)])]
+  names(last_data) <- c("ID","Time","delta",covr_names)
+  
+  ##  Check the types of covariate(s)
+  if(nrow(last_data)==nrow(unique(data[,c(names(data)[1],covr_names)]))){
+    
+    # 1. Time-independent covariate(s) type : survreg modeling
+    formula_indep <- stats::as.formula(paste0("Surv(Time,delta)~",paste(covr_names, collapse="+")))
+    model_result <- survival::survreg(formula_indep,data=last_data,dist="lognormal")
+    Betas_results <- round(as.numeric(c(model_result$coefficients,model_result$scale)),4)
+    names(Betas_results) <- c(paste("Beta",c(0:length(covr_names)),sep=""),"Scale")
+    final <- list(EstBetas=model_result$coefficients,Model="Time-independent AFT")
+    
   }else{
-    dat1 <- as.data.frame(dat%>%group_by(ID)%>%arrange(stop)%>%slice(n()))
-    X.mat <- as.matrix(dat1[,X])
-    PAFT <- survreg(Surv(dat1[,"stop"],dat1[,"delta"]) ~ X.mat,dist="lognormal")
-    beta <- PAFT$coefficients # initial betas
-    gab <- PAFT$scale  # initial support set( = SE)
-  }
-
-  if(dist=="log_normal"){
-    ## optimization function
-    PAFT_opt <- function(theta){
-
-      ## 1. Calculation of the observation time
-      obs_time <- as.data.frame(dat%>%group_by(ID)%>%slice(n()))[,c("ID","stop")]
-      names(obs_time) <- c("ID","obs_time")
-      dat <- merge(dat,obs_time,by="ID",all.x=T)
-      dat <- dat[order(dat$ID,dat$start),] # ordering
-
-
-      ## 3. Likelihood function of the AFT with time-depnedent
-
-      ## Term1 : Calculation of the Capital psi
-      # : integrated from 0 to yi(observed time) of exp(-z(u)*beat^T)
-
-
-      ## 1.
-      # AA <- -theta[2]*dat[,X[1]]-theta[3]*dat[,X[2]]
-
-      ## 2.
-      # A1 <- -theta[2]*dat[,X[1]]
-      # A2 <- -theta[3]*dat[,X[2]]
-      # AA <- A1+A2
-
-      ## 3.
-      AA <- 0
-      for(i in 1:length(X)){
-        AA <- -theta[i+1]*dat[,X[i]]+AA
-      }
-
-      dat$psi <- (dat$stop-dat$start)*exp(AA)
-      # dat$psi <- (dat$stop-dat$start)*exp(rowSums(-theta[2:(1+length(X))] *dat[,X]))
-      final <- aggregate(dat[,c("psi")],list(dat[,"ID"]),FUN=sum)
-      names(final) <- c("ID","psi")
-      final <- final[order(final$ID),] # ordering
-
-
-      ## Term2 : Calculation of psi : exp(-beta*z(yi))
-      last_dat <- as.data.frame(dat%>%group_by(ID)%>%slice(n()))[,c("ID",X)]
-      BB <- 0
-      for(i in 1:length(X)){
-        BB <- -theta[i+1]*last_dat[,X[i]]+BB
-      }
-      final$psi_d <- exp(BB)
-      # final$psi_d <- exp(-theta[2]*last_dat[,"Z1"]-theta[3]*last_dat[,"X"])
-      # final$psi_d <- exp(rowSums(-theta[2:(1+length(X))] *last_dat[,X]))
-      final <- final[order(final$ID),] # ordering
-
-      ## Joint delta
-      delta <- as.data.frame(dat%>%group_by(ID)%>%slice(n()))[,c("ID","delta")]
-      final <- merge(final,delta,by="ID",all.x=T)
-      final <- final[order(final$ID),] # ordering
-
-      ## likelihood function ##
-      sigma <- theta[length(theta)]
-      logPsi <- log(final$psi)
-      dnorm_2 <- dnorm((logPsi-theta[1])/sigma)*(final$psi_d/(sigma*final$psi))
-      S <- final$delta*dnorm_2 + (1-final$delta)*(1 - pnorm((logPsi-theta[1])/sigma))
-      S <- ifelse(S<=1.0e-20,1.0e-20,S)
-
-      lik <- sum(log(S))
-      lik_inv <- -lik
-      return(lik_inv)
+    
+    # 2. Time-dependent covariate(s) type : using optimization method
+    
+    
+    # initial values: beta0 and scale0
+    if(initial==T){
+      beta <- beta0; scale <- scale0
+    }else{
+      formula_indep <- stats::as.formula(paste0("Surv(Time,delta)~",paste(covr_names, collapse="+")))
+      PAFT <- survival::survreg(formula_indep,data=last_data,dist="lognormal")
+      beta <- PAFT$coefficients;scale <- PAFT$scale
     }
-
-    # initial values
-    # b <- c(1e-20,rep(1e-20,length(X)),1e-20)
-    # b <- c(1,rep(1,length(X)),1)
-    b <- as.numeric(c(beta,gab))
-
-    # bounded values
-    bound <- c(max(abs(b))*10,rep(max(abs(b))*10,length(X)),max(abs(b))*10)
-    # bound <- c(Inf,rep(Inf,length(X)),Inf)
-
-
-    # ----  nloptr function ----#
-    res2 <- nloptr(x0=b,eval_f=PAFT_opt,lb = -bound,ub = bound,
-                   opts = list("algorithm"=algorithm,"xtol_rel"=tol,maxeval=maxiter))
-    # Results
-    Betas_results <- res2$solution[1:(1+length(X)+1)] # Estimated betas
-    names(Betas_results) <- c(paste("b",c(0:length(X)),sep=""),"sigma")
-    itr <- res2$iterations # number of iterations of the algorithm
-    status <- res2$status # integer value with the status of the optimization
-    message <- res2$message # more informative message with the status of the optimization
-    # --------------------------#
-
-    # ----  optimum function ----#
-    # res2 <- optim(b,PAFT_opt,lower=-bound,upper=bound,method = "L-BFGS-B")
-
-    # # Results
-    # Betas_results <- res2$par[1:(1+length(X)+1)] # Estimated betas
-    # names(Betas_results) <- c(paste("b",c(0:length(X)),sep=""),"sigma")
-    # message <- res2$message
-    # convergence <- res2$convergence
-    # count <- res2$counts
-
-    # A <- list(EstBetas=Betas_results,count=count,convergence=convergence,
-    #           message=message)
-    A <- list(EstBetas=Betas_results,itr=itr,status=status,
-              message=message)
-  }else{
-    A <- NULL
+    
+    
+    # dist1: log-normal distribution
+    if(dist=="lognormal"){
+      
+      # Optimization function
+      PAFT_opt <- function(theta){
+        
+        # 1. Calculation of the observation time
+        dat <- data[,c(names(data)[1],varnames)]
+        names(dat) <- c("ID",varnames)
+        obs_time <- last_data[,c("ID","Time")];names(obs_time) <- c("ID","obs_time")
+        dat <- merge(dat,obs_time,by="ID",all.x=T)
+        dat <- dat[order(dat$ID,dat$start),] # ordering
+        
+        # 2. Likelihood function of the PAFT with time-dependent covariates
+        
+        ## Term1 : Calculation of the Capital psi 
+        AA <- 0
+        for(i in 1:length(covr_names)){
+          AA <- -theta[i+1]*dat[,covr_names[i]]+AA
+        }
+        dat$psi <- (dat$stop-dat$start)*exp(AA)
+        final <- as.data.frame(dat%>%group_by(ID)%>%summarise(psi=sum(psi)))
+        
+        ## Term2 : Calculation of psi : exp(-beta*z(yi))
+        BB <- 0
+        for(i in 1:length(covr_names)){
+          BB <- -theta[i+1]*last_data[,covr_names[i]]+BB
+        }
+        final$psi_d <- exp(BB)
+        
+        ## Joint delta
+        delta <- last_data[,c("ID","delta")]
+        final <- merge(final,delta,by="ID",all.x=T)
+        final <- final[order(final$ID),] # ordering
+        
+        ## likelihood function ## 
+        scale <- theta[length(theta)]
+        logPsi <- log(final$psi)
+        dnorm_2 <- dnorm((logPsi-theta[1])/scale)*(final$psi_d/(scale*final$psi))
+        S <- final$delta*dnorm_2 + (1-final$delta)*(1 - pnorm((logPsi-theta[1])/scale))
+        S <- ifelse(S<=1.0e-20,1.0e-20,S)
+        
+        lik <- sum(log(S))
+        lik_inv <- -lik
+        return(lik_inv)
+      }
+      
+      b <- as.numeric(c(beta,scale))
+      
+      # bounded values
+      bound <- c(max(abs(b))*10,rep(max(abs(b))*10,length(covr_names)),max(abs(b))*10)
+      
+      # ----  nloptr function ----#
+      res2 <- nloptr(x0=b,eval_f=PAFT_opt,lb = -bound,ub = bound,
+                     opts = list("algorithm"=algorithm,"xtol_rel"=tol,maxeval=maxiter))
+      
+      # Results
+      Betas_results <- res2$solution[1:(1+length(covr_names)+1)] # Estimated betas
+      names(Betas_results) <- c(paste("Beta",c(0:length(covr_names)),sep=""),"Scale")
+      itr <- res2$iterations # number of iterations of the algorithm
+      status <- res2$status # integer value with the status of the optimization
+      message <- res2$message # more informative message with the status of the optimization
+      
+      final <- list(EstBetas=Betas_results,itr=itr,status=status,
+                    message=message)
+    }else{
+      final <- NULL
+    }
   }
-
-  return(A)
+  return(final)
 }
